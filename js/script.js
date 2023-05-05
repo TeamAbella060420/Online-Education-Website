2b652ebd29fa0425a6bd4061efe84434a9e5bf75
TeamAbella060420 <teamabella@outlook.com>
2023-05-05 18:18:30 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
 function toggleTheme() {
     const sunIcon = document.querySelector("#theme-toggle .fa-sun");
     const moonIcon = document.querySelector("#theme-toggle .fa-moon");
-    
+
     if (sunIcon.style.display === "none") {
-      sunIcon.style.display = "block";
-      moonIcon.style.display = "none";
+        sunIcon.style.display = "block";
+        moonIcon.style.display = "none";
     } else {
-      sunIcon.style.display = "none";
-      moonIcon.style.display = "block";
+        sunIcon.style.display = "none";
+        moonIcon.style.display = "block";
     }
-  }
-  
-  const themeToggle = document.querySelector("#theme-toggle");
-  themeToggle.addEventListener("click", toggleTheme);
-  
+}
+
+const themeToggle = document.querySelector("#theme-toggle");
+themeToggle.addEventListener("click", toggleTheme);
