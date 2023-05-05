2a8ac996e480a590d1456e1a234f0b723f01b80d
TeamAbella060420 <teamabella@outlook.com>
2023-05-05 20:58:36 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Toggle between light and dark themes when theme toggle is clicked
+
 function toggleTheme() {
     const sunIcon = document.querySelector("#theme-toggle .fa-sun");
     const moonIcon = document.querySelector("#theme-toggle .fa-moon");
@@ -13,3 +15,16 @@ function toggleTheme() {
 
 const themeToggle = document.querySelector("#theme-toggle");
 themeToggle.addEventListener("click", toggleTheme);
+
+// Toggle visibility of profile when user icon is clicked
+
+const profileToggle = document.querySelector(".header .fas.fa-user");
+const profile = document.querySelector(".profile");
+
+profileToggle.addEventListener("click", function() {
+  if (profile.style.display === "none") {
+    profile.style.display = "block";
+  } else {
+    profile.style.display = "none";
+  }
+});
