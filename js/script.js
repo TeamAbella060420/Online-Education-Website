bce01a32b4e929db61d9af281f666d0a403aece3
TeamAbella060420 <teamabella@outlook.com>
2023-05-06 13:02:00 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,13 +18,23 @@ themeToggle.addEventListener("click", toggleTheme);
 
 // Toggle visibility of profile when user icon is clicked
 
-const profileToggle = document.querySelector(".header .fas.fa-user");
-const profile = document.querySelector(".profile");
+// const profileToggle = document.querySelector(".header .fas.fa-user");
+// const profile = document.querySelector(".profile");
 
-profileToggle.addEventListener("click", function() {
-  if (profile.style.display === "none") {
-    profile.style.display = "block";
-  } else {
-    profile.style.display = "none";
-  }
-});
+// profileToggle.addEventListener("click", function() {
+//   if (profile.style.display === "none") {
+//     profile.style.display = "block";
+//   } else {
+//     profile.style.display = "none";
+//   }
+// });
+
+let profile = document.querySelector('.header .flex .profile');
+
+document.querySelector('#user-btn').onclick = () => {
+  profile.classList.toggle('active')
+}
+
+window.onscroll = () => {
+  profile.classList.toggle('active')
+}
