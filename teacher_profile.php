afa39b7accfe176c60401991833267e4fbf7a13a
TeamAbella060420 <teamabella@outlook.com>
2023-05-19 13:33:58 +0800

    Update teacher_profile.php

--- teacher_profile.php
+++ teacher_profile.php
@@ -20,7 +20,7 @@
     <!-- header section starts -->
     <header class="header">
       <section class="flex">
-        <a href="home.html" class="logo">Educate</a>
+        <a href="home.php" class="logo">Educate</a>
         <form action="" method="post" class="search-form">
           <input
             type="text"
@@ -48,10 +48,10 @@
           <img src="images/pic-1.jpg" alt="" />
           <h3>Abraham Abella</h3>
           <span>Student</span>
-          <a href="profile.html" class="btn">View profile</a>
+          <a href="profile.php" class="btn">View profile</a>
           <div class="flex-btn">
-            <a href="login.html" class="option-btn">login</a>
-            <a href="register.html" class="option-btn">register</a>
+            <a href="login.php" class="option-btn">login</a>
+            <a href="register.php" class="option-btn">register</a>
           </div>
         </div>
       </section>
@@ -69,21 +69,21 @@
         <img src="images/pic-1.jpg" alt="" />
         <h3>Abraham Abella</h3>
         <span>Student</span>
-        <a href="profile.html" class="btn">View profile</a>
+        <a href="profile.php" class="btn">View profile</a>
       </div>
 
       <nav class="navbar">
-        <a href="home.html"><i class="fas fa-home"></i><span>Home</span></a>
-        <a href="about.html"
+        <a href="home.php"><i class="fas fa-home"></i><span>Home</span></a>
+        <a href="about.php"
           ><i class="fas fa-question"></i><span>About us</span></a
         >
-        <a href="courses.html"
+        <a href="courses.php"
           ><i class="fas fa-graduation-cap"></i><span>Our Courses</span></a
         >
-        <a href="teachers.html"
+        <a href="teachers.php"
           ><i class="fas fa-chalkboard-user"></i><span>Teachers</span></a
         >
-        <a href="contact.html"
+        <a href="contact.php"
           ><i class="fas fa-headset"></i><span>Contact US</span></a
         >
       </nav>
@@ -128,7 +128,7 @@
           </div>
           <img src="images/thumb-1.png" class="thumb" alt="" />
           <h3 class="title">Complete HTML Tutorial</h3>
-          <a href="playlist.html" class="inline-btn">View Playlist</a>
+          <a href="playlist.php" class="inline-btn">View Playlist</a>
         </div>
 
         <div class="box">
@@ -141,7 +141,7 @@
           </div>
           <img src="images/thumb-2.png" class="thumb" alt="" />
           <h3 class="title">Complete CSS Tutorial</h3>
-          <a href="playlist.html" class="inline-btn">View Playlist</a>
+          <a href="playlist.php" class="inline-btn">View Playlist</a>
         </div>
 
         <div class="box">
@@ -154,7 +154,7 @@
           </div>
           <img src="images/thumb-3.png" class="thumb" alt="" />
           <h3 class="title">Complete JS Tutorial</h3>
-          <a href="playlist.html" class="inline-btn">View Playlist</a>
+          <a href="playlist.php" class="inline-btn">View Playlist</a>
         </div>
 
         <div class="box">
@@ -167,7 +167,7 @@
           </div>
           <img src="images/thumb-4.png" class="thumb" alt="" />
           <h3 class="title">Complete Bootstrap Tutorial</h3>
-          <a href="playlist.html" class="inline-btn">View Playlist</a>
+          <a href="playlist.php" class="inline-btn">View Playlist</a>
         </div>
       </div>
     </section>
